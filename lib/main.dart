b35742f4fd4e7696f7838610ae120c06047cfaaa
Ed-Movchan <iamtarx@gmail.com>
2023-01-04 19:13:17 +0100

    Change title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Web Site',
+      title: 'Movchan Eduard - System Administrator',
       initialRoute: '/',
       routes: {
         '/': (context) => MyHomePage(),
